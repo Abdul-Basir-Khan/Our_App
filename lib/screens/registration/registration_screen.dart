f77a8ce2279440097dc60817abf99ee0579596d1
Abdul-Basir-Khan <abdulbak750@gmail.com>
2023-12-24 00:20:51 +0500

    "first commit"

--- lib/screens/registration/registration_screen.dart
+++ lib/screens/registration/registration_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:our_app/themes/app_colors.dart';
+import 'package:our_app/themes/app_textStyle.dart';
 
 
 class RegistrationScreen extends StatefulWidget {
@@ -11,6 +13,46 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      extendBody: true,
+      body: ListView(
+      children: [
+        Container(
+
+          height:  MediaQuery.sizeOf(context).height*0.25,
+          width: MediaQuery.sizeOf(context).width*1,
+          padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              AppColors.mainColor,
+              AppColors.mainLight,
+            ]
+
+            ),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight:  Radius.circular(20))
+          ),
+          child: Column(
+            children: [
+              Icon(Icons.arrow_back,color: AppColors.primaryWhite,),
+              Center(child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 Text(
+                   'Sign Up',
+                   style: AppTextStyles.mediumStyle.copyWith(color: AppColors.primaryWhite),
+                 ),
+                 Text(
+                   'Sign In',
+                   style: AppTextStyles.mediumStyle.copyWith(color: AppColors.primaryWhite),
+                 ),
+                ],
+              ),)
+            ],
+          ),
+        )
+
+
+      ],
+    ),);
   }
 }
